99d9a86db28cde4c1d01b59fd30f2b487db77a56
Jason Reed <jcreed@gmail.com>
2020-04-12 18:32:15 -0400

    attempt to get working on glitch

--- prod-server.ts
+++ prod-server.ts
@@ -1,6 +1,8 @@
 import { start } from './core-server';
 import * as express from 'express';
 
+console.log('starting production server');
+
 start(app => {
   app.use('/js/bundle.js', express.static(__dirname + '/public/js/bundle.js'));
 });
